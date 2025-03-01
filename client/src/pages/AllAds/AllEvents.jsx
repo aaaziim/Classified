bf5e5e6a68aaf4b19651fac658f371f55bef4396
Azim <azim210215@gmail.com>
2025-03-02 02:33:33 +0600

    Filter by category and subcategory added

--- client/src/pages/AllAds/AllEvents.jsx
+++ client/src/pages/AllAds/AllEvents.jsx
@@ -5,6 +5,7 @@ import { Helmet } from 'react-helmet-async'
 import Breadcrumb from '../Components/Breadcrumb'
 import useAxiosSecure from '../../hooks/useAxiosSecure'
 import EventCard from '../Components/EventCard'
+import Pagination from '../Components/Pagination'
 
 const AllEvents = () => {
 
@@ -108,17 +109,7 @@ if (errorEvents) return <div className="text-center text-[#FA8649]">{errorEvents
               </button>
             </div>
           </form>
-          <div>
-            <select
-              name='category'
-              id='category'
-              className='border p-4 rounded-md'
-            >
-              <option value=''>Sort By Deadline</option>
-              <option value='dsc'>Descending Order</option>
-              <option value='asc'>Ascending Order</option>
-            </select>
-          </div>
+    
           <button className='btn'>Reset</button>
         </div>
        
@@ -130,58 +121,7 @@ if (errorEvents) return <div className="text-center text-[#FA8649]">{errorEvents
          }
            </div>
 
-      <div className='flex justify-center mt-12'>
-        <button className='px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white'>
-          <div className='flex items-center -mx-1'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='w-6 h-6 mx-1 rtl:-scale-x-100'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M7 16l-4-4m0 0l4-4m-4 4h18'
-              />
-            </svg>
-
-            <span className='mx-1'>previous</span>
-          </div>
-        </button>
-
-        {pages.map(btnNum => (
-          <button
-            key={btnNum}
-            className={`hidden px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
-          >
-            {btnNum}
-          </button>
-        ))}
-
-        <button className='px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500'>
-          <div className='flex items-center -mx-1'>
-            <span className='mx-1'>Next</span>
-
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='w-6 h-6 mx-1 rtl:-scale-x-100'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M17 8l4 4m0 0l-4 4m4-4H3'
-              />
-            </svg>
-          </div>
-        </button>
-      </div>
+      <Pagination></Pagination>
     
     </div>
       
--- client/src/pages/AllAds/AllServices.jsx
+++ client/src/pages/AllAds/AllServices.jsx
@@ -4,9 +4,13 @@ import AdCard from '../Components/AdCard'
 import { Helmet } from 'react-helmet-async'
 import Breadcrumb from '../Components/Breadcrumb'
 import useAxiosSecure from '../../hooks/useAxiosSecure'
+import Pagination from '../Components/Pagination'
+import { useNavigate } from 'react-router'
+import useAuth from '../../hooks/useAuth'
 
 const AllServices = () => {
 
+  const {user} = useAuth()
   const [services, setServices] = useState([]);
   const [events, setEvents] = useState([]);
 
@@ -19,9 +23,56 @@ const [errorEvents, setErrorEvents] = useState('');
 
 
 
+
+
+  const [locations, setLocations] = useState([]);
+  const [country, setCountry] = useState("");
+  const [stateIndex, setStateIndex] = useState(0);
+  const [category, setCategory] = useState(null);
+  const [subcategory, setSubCategory] = useState(null);
+  const [categoryIndex, setCategoryIndex] = useState(0);
+  const [state, setState] = useState();
+const [categories, setCategories] = useState([]);
+const [loadingCategories, setLoadingCategories] = useState(true);
+const [loadingLocations, setLoadingLocations] = useState(true);
+const [errorCategories, setErrorCategories] = useState('');
+const [errorLocations, setErrorLocations] = useState('');
+ 
+const navigate = useNavigate()
+
+
+
+
+
+
+
 useEffect(() => {
  
+  const fetchCategories = async () => {
+    try {
+      // Fetch categories from the API endpoint using the secure axios instance
+      const response = await axiosSecure("/categories")
+      setCategories(response.data);
+      setLoadingCategories(false);
+    } catch (err) {
+      setErrorCategories('Error loading categories');
+      setLoadingCategories(false);
+    }
+  };
 
+  const fetchLocations = async () => {
+    try {
+      const response = await axiosSecure("/locations")
+      
+      setLocations(response.data);
+      setLoadingLocations(false);
+    } catch (err) {
+      setErrorLocations('Error loading locations');
+      setLoadingLocations(false);
+    }
+  };
+
+  
 
   const fetchServices = async () => {
     try {
@@ -35,10 +86,39 @@ useEffect(() => {
     }
   };
  
-
+  fetchCategories();
+  fetchLocations();
   fetchServices();
   
 }, []);
+
+
+const fetchServicesbyCategory = async (name) => {
+  console.log(name)
+  try {
+    // Fetch categories from the API endpoint using the secure axios instance
+    const response = await axiosSecure(`/servicesbycategory?category=${encodeURIComponent(name)}`);
+    setServices(response.data);
+    setLoadingServices(false);
+  } catch (err) {
+    setErrorServices('Error loading services');
+    setLoadingServices(false);
+  }
+};
+
+
+const fetchServicesbySubCategory = async (name,category) => {
+  console.log(category, name)
+  try {
+    // Fetch categories from the API endpoint using the secure axios instance
+    const response = await axiosSecure(`/servicesbysubcategory?category=${encodeURIComponent(category)}&subcategory=${encodeURIComponent(name)}`);
+    setServices(response.data);
+    setLoadingServices(false);
+  } catch (err) {
+    setErrorServices('Error loading services');
+    setLoadingServices(false);
+  }
+};
 
 
 
@@ -51,18 +131,6 @@ if (errorServices) return <div className="text-center text-[#FA8649]">{errorServ
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-  const pages = [1, 2, 3, 4, 5]
   return (
     <div>
         <Helmet>
@@ -74,113 +142,109 @@ if (errorServices) return <div className="text-center text-[#FA8649]">{errorServ
        subTitle={"Here you can find"}>
        </Breadcrumb>
     </div>
+    <div><div>
+    <form className="flex justify-center">
+  <div className="flex p-1 border rounded-md w-64 mx-auto focus-within:ring focus-within:ring-[#FA8649]">
+    <input
+      className="px-2 py-1 w-full text-gray-700 placeholder-gray-500 bg-white outline-none text-sm"
+      type="text"
+      name="search"
+      placeholder="Search..."
+      aria-label="Search"
+    />
+    <button className="px-3 py-1 text-xs font-medium text-white bg-[#014D48] rounded-md hover:bg-[#FA8649] transition">
+      Search
+    </button>
+  </div>
+</form>
+
+          </div></div>
          
         <div className='container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between'>
-      <div>
-        <div className='flex flex-col md:flex-row justify-center items-center gap-5 '>
+          
+        <div className="flex flex-col lg:flex-row justify-between gap-6">
+          
+        <div className=' '>
           <div>
-            <select
-              name='category'
-              id='category'
-              className='border p-4 rounded-lg'
-            >
-              <option value=''>Filter By Category</option>
-              <option value='Web Development'>Web Development</option>
-              <option value='Graphics Design'>Graphics Design</option>
-              <option value='Digital Marketing'>Digital Marketing</option>
-            </select>
+          <select name='service_category' className='mt-1 block w-full border rounded-lg p-2 focus:ring focus:ring-[#FA8649]' required
+                             onChange={(e) => {
+                              setServices([])
+                              setCategory(e.target.value)
+                              fetchServicesbyCategory(e.target.value)
+                              setCategoryIndex(e.target.selectedIndex - 1)
+                             }} 
+                            >
+                                <option value=''>Select Category</option>
+                                {
+    categories.map((category, index) => (
+        <option 
+            key={index} 
+            value={category.name} 
+           // Wrap the function call in an anonymous function
+        >
+            {category.name}
+        </option>
+    ))
+}
+                            </select>
+          </div>
+         {
+          category &&  <div>
+          <select name='service_subcategory' 
+          onChange={(e)=>{
+            setServices([])
+            setSubCategory(e.target.value)
+            fetchServicesbySubCategory(e.target.value, category)
+          }} className='mt-1 block w-full border rounded-lg p-2 focus:ring focus:ring-[#FA8649]' required>
+                                <option value=''>Select Sub-Category</option>
+                                {
+
+categories[categoryIndex]?.subcategories?.map((subcategory, index) => (
+    <option 
+        key={index} 
+        value={subcategory.name} 
+    >
+        {subcategory.name}
+    </option>
+))
+                                }
+                            </select>
           </div>
 
-          <form>
-            <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
-              <input
-                className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
-                type='text'
-                name='search'
-                placeholder='Enter Job Title'
-                aria-label='Enter Job Title'
-              />
-
-              <button className='px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
-                Search
-              </button>
-            </div>
-          </form>
-          <div>
-            <select
-              name='category'
-              id='category'
-              className='border p-4 rounded-md'
-            >
-              <option value=''>Sort By Deadline</option>
-              <option value='dsc'>Descending Order</option>
-              <option value='asc'>Ascending Order</option>
-            </select>
-          </div>
-          <button className='btn'>Reset</button>
-        </div>
+         }
         
-      </div>
-       
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-4">
+    
+         
+        </div>
+
+
+
+
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-4 flex-1">
       {
           services.map((service, index) =><AdCard key={index} service={service}></AdCard>)
          }
            </div>
 
-      <div className='flex justify-center mt-12'>
-        <button className='px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white'>
-          <div className='flex items-center -mx-1'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='w-6 h-6 mx-1 rtl:-scale-x-100'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M7 16l-4-4m0 0l4-4m-4 4h18'
-              />
-            </svg>
 
-            <span className='mx-1'>previous</span>
+
+
+
+
+
+
+
           </div>
-        </button>
-
-        {pages.map(btnNum => (
-          <button
-            key={btnNum}
-            className={`hidden px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
-          >
-            {btnNum}
-          </button>
-        ))}
-
-        <button className='px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500'>
-          <div className='flex items-center -mx-1'>
-            <span className='mx-1'>Next</span>
-
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='w-6 h-6 mx-1 rtl:-scale-x-100'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M17 8l4 4m0 0l-4 4m4-4H3'
-              />
-            </svg>
-          </div>
-        </button>
+         
+        
+      <div>
+       
       </div>
-    
+       
+  
+
+    <Pagination></Pagination>
     </div>
       
  
