48074d8fbfbbd1fafdef7af1da3c48aee4582995
Azim <azim210215@gmail.com>
2025-03-16 09:16:13 +0600

    Admin Dashboad Restricted

--- client/src/pages/Admin/Dashboard/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard/Dashboard.jsx
@@ -247,9 +247,9 @@ const Dashboard = () => {
   if (errorEvents) return <div className="text-center text-[#FA8649]">{errorEvents}</div>;
   
 
-  // if (!isAdmin) {
-  //   return <Navigate to="/" />;
-  // }
+  if (!isAdmin) {
+    return <Navigate to="/" />;
+  }
   
 
 
